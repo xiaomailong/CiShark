9eb0d4e47215b9a02558b055021f371c8ff9cbf4
lisency <zh-y-sh@qq.com>
2015-04-21 14:24:06 +0800

    add log_type for debug sql

--- src/DebugSql/select_range_time.sql
+++ src/DebugSql/select_range_time.sql
@@ -1,4 +1,4 @@
-SELECT session_id,FROM_UNIXTIME(log_tv_sec),log_tv_usec,get_log_type_name(log_type),log_content
+SELECT session_id,FROM_UNIXTIME(log_tv_sec),log_tv_usec,get_log_type_name(log_type),log_type,log_content
 FROM `log20150323`
 WHERE session_id = 138 
 	AND log_type IN (165,166,167,168,169,170,171,0) 
