214cfa8ce82dc69b4da7c7eeec806e49980f92cf
lisency <zh-y-sh@qq.com>
2015-07-17 08:33:04 +0800

    delete unuse code blocks

--- main.py
+++ main.py
@@ -2,12 +2,6 @@
 
 from cishark import RecordServer
 
-import numpy as np
-
-a = np.array()
-
-import dis
-dis.dis
 if __name__ == "__main__":
     recorder = RecordServer(port = 3003)
     recorder.serve_forever()
